ef753f7ca19c632aec28a92f87bd18ced2c949d4
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-30 10:22:46 -0500

    test

--- test/test.sh
+++ test/test.sh
@@ -6,5 +6,5 @@ cd test
 gem install chef --version 10.12.0
 gem install librarian --version 0.0.24
 librarian-chef install
-sudo sed -i "s/%admin ALL=NOPASSWD:ALL/%admin ALL=\(ALL\) NOPASSWD:ALL/" /etc/sudoers
-sudo -u vagrant chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json
+#sudo sed -i "s/%admin ALL=NOPASSWD:ALL/%admin ALL=\(ALL\) NOPASSWD:ALL/" /etc/sudoers
+chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json

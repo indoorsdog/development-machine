392da245d6eb168b5686ee9a4a3ce6b55bfd86cb
vagrant <vagrant@development.(none)>
2012-07-30 03:50:44 +0000

    test

--- test/test.sh
+++ test/test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd test
-sudo gem install chef --version 10.12.0
-sudo gem install librarian --version 0.0.24
-sudo librarian-chef install
+gem install chef --version 10.12.0
+gem install librarian --version 0.0.24
+librarian-chef install
 sudo chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json

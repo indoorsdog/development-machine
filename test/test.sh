890c99f315dec3e86c28c7bcff82eb50e33a5f4e
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-30 16:34:14 -0500

    test

--- test/test.sh
+++ test/test.sh
@@ -7,6 +7,6 @@ rvmsudo gem install chef --version 10.12.0
 rvmsudo gem install librarian --version 0.0.24
 rvmsudo librarian-chef install
 rvmsudo chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json
-rvmsudo vagrant --version
+vagrant --version
 cd ..
 vagrant up

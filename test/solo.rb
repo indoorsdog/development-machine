6c9e7f709d4db0ea54cf23d572b373bed0586240
vagrant <vagrant@development.(none)>
2012-07-30 03:29:00 +0000

    test

---
+++ test/solo.rb
@@ -0,0 +1,2 @@
+
+cookbook_path ['/home/vagrant/builds/indoorsdog/development-machine/test/cookbooks', '/home/vagrant/builds/indoorsdog/development-machine/test/site-cookbooks']
--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd test
 gem install chef --version 10.12.0
-chef-solo
-pwd
+gem install librarian --version 0.0.24
+librarian-chef install
+chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.rb

ed5c4bcc6a41540d0c66d2641f3a797c4b67853f
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-30 10:38:35 -0500

    test

--- test/test.sh
+++ test/test.sh
@@ -7,5 +7,5 @@ gem install chef --version 10.12.0
 gem install librarian --version 0.0.24
 librarian-chef install
 #sudo sed -i "s/%admin ALL=NOPASSWD:ALL/%admin ALL=\(ALL\) NOPASSWD:ALL/" /etc/sudoers
-sudo apt-get update
+sudo chmod 777 /etc/apt/sources.list
 chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json

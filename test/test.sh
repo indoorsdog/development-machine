20f067a758d38d44bec419f4c7a1741b3b1981e8
vagrant <vagrant@development.(none)>
2012-07-30 23:50:29 +0000

    test

--- test/test.sh
+++ test/test.sh
@@ -7,4 +7,4 @@ lsb_release -irc
 sudo apt-get install --assume-yes virtualbox
 wget http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/vagrant_1.0.3_i686.deb
 sudo dpkg -i vagrant_1.0.3_i686.deb
-ls
+vagrant --version

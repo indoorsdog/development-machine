c443d640ebb94eeb2526207142150198195b71cb
vagrant <vagrant@development.(none)>
2012-07-30 23:26:44 +0000

    test

--- test/test.sh
+++ test/test.sh
@@ -3,10 +3,4 @@
 whoami
 pwd
 cd test
-rvmsudo gem install chef --version 10.12.0
-rvmsudo gem install librarian --version 0.0.24
-rvmsudo librarian-chef install
-rvmsudo chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json
-vagrant --version
-cd ..
-vagrant up
+lsb_release -irc

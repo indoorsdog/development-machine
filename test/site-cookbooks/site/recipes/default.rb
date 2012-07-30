1317f774795f0f72d687d880e8e4aa5901146152
vagrant <vagrant@development.(none)>
2012-07-30 20:12:48 +0000

    test

---
+++ test/site-cookbooks/site/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: site
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+remote_file '/home/vagrant/builds/indoorsdog/development-machine/test/vagrant_1.0.3_i686.deb' do
+  source 'http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/vagrant_1.0.3_i686.deb'
+end
+
+dpkg_package 'vagrant' do
+  source 'http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/vagrant_1.0.3_i686.deb'
+end
--- test/test.sh
+++ test/test.sh
@@ -6,8 +6,4 @@ cd test
 rvmsudo gem install chef --version 10.12.0
 rvmsudo gem install librarian --version 0.0.24
 rvmsudo librarian-chef install
-# yes this sudo stuff is nonsense, but I just want to get it to run
-#sudo chmod 777 -R /etc/apt
-#sudo chmod 777 -R /var/lib
-#sudo chmod 777 -R /var/cache
 rvmsudo chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json

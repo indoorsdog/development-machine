4f36762fedbbaa291ca04e9ce5c04d72511b6e93
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-30 10:59:19 -0500

    test

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,8 @@ cd test
 gem install chef --version 10.12.0
 gem install librarian --version 0.0.24
 librarian-chef install
+# yes this sudo stuff is nonsense, but I just want to get it to run
 sudo chmod 777 -R /etc/apt
 sudo chmod 777 -R /var/lib
+sudo chmod 777 -R /var/cache
 chef-solo --config ~/builds/indoorsdog/development-machine/test/solo.rb --json-attributes ~/builds/indoorsdog/development-machine/test/node.json

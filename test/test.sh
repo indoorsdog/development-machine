13b6b87cc6fae5d34b7bd517359a957e1116b490
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-31 18:19:35 -0500

    Issue #4: evaluate return values from vagrant commands

--- test/test.sh
+++ test/test.sh
@@ -8,7 +8,22 @@ sudo apt-get install --assume-yes virtualbox
 wget http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000a/vagrant_1.0.3_i686.deb
 sudo dpkg -i vagrant_1.0.3_i686.deb
 cd ..
+
+OUT=$?
+if [ $OUT -eq 0 ];then
+       echo "User account found!"
+   else
+          echo "User account does not exists in /etc/passwd file!"
+      fi
 /opt/vagrant/bin/vagrant up
+OUT=$?
+if [ $OUT -ne 0 ];then
+  exit 1
+fi
 /opt/vagrant/bin/vagrant ssh --command 'touch /vagrant/test/created_in_vm'
+OUT=$?
+if [ $OUT -ne 0 ];then
+  exit 1
+fi
 ls test
 [ -f test/created_in_vm ] && exit 0 || exit 1

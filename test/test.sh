b910bd6f68cd8f24153c4f9f908215e29904e414
Luke Patterson <lukewpatterson@Lukes-MacBook-Pro.local>
2012-07-30 19:37:25 -0500

    test

--- test/test.sh
+++ test/test.sh
@@ -9,7 +9,7 @@ wget http://files.vagrantup.com/packages/eb590aa3d936ac71cbf9c64cf207f148ddfc000
 sudo dpkg -i vagrant_1.0.3_i686.deb
 cd ..
 /opt/vagrant/bin/vagrant up
-/opt/vagrant/bin/vagrant ssh --command 'touch /vagrant/test/created_in_vm''
+/opt/vagrant/bin/vagrant ssh --command 'touch /vagrant/test/created_in_vm'
 ls test
 if [ -f test/created_in_vm ]
 then

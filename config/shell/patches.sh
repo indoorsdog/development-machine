87723252cafc8f395b3a52966d005876480bd2f2
Steven MountJoy <ecc11718@gmail.com>
2012-08-09 14:29:58 -0500

    conditional installation of git and librarian-chef.

--- config/shell/patches.sh
+++ config/shell/patches.sh
@@ -11,13 +11,24 @@ else
     wget -nv -O - http://opscode.com/chef/install.sh | sudo bash
 fi
 
+# git
+gitVer=`git --version`
+if [[ $gitVer =~ 1\.7\.9\.5 ]]
+then
+    echo git version up-to-date. not updating.
+else
+    "$gitVer. updating..."
+    sudo apt-get install git=1:1.7.9.5-1 --assume-yes
+fi
+
 #download chef cookbooks
 #https://github.com/applicationsonline/librarian
-sudo apt-get install git=1:1.7.9.5-1 --assume-yes
-gem  install librarian --version 0.0.24
-mkdir /usr/local/chef-repo
-cp /vagrant/config/chef-repo/Cheffile /usr/local/chef-repo/Cheffile
-cp /vagrant/config/chef-repo/Cheffile.lock /usr/local/chef-repo/Cheffile.lock
-cd /usr/local/chef-repo
-librarian-chef install --verbose
-
+if [ ! `which librarian-chef` ]; then
+    "installing librarian-chef..."
+    gem  install librarian --version 0.0.24
+    mkdir /usr/local/chef-repo
+    cp /vagrant/config/chef-repo/Cheffile /usr/local/chef-repo/Cheffile
+    cp /vagrant/config/chef-repo/Cheffile.lock /usr/local/chef-repo/Cheffile.lock
+    cd /usr/local/chef-repo
+    librarian-chef install --verbose
+fi
